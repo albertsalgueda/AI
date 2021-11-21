41898a3266f41d290a653a2924d1070209fea573
albertsalgueda <albert.salgueda01@estudiant.upf.edu>
2021-11-21 16:19:27 -0500

    virtual

---
+++ VirtualAssistant/.venv/xpl.py
@@ -0,0 +1,14 @@
+#Listen to what I am saying -> Give a basic command
+#Speak -> Answer
+import speech_recognition as sr
+listener = sr.Recognizer()
+try:
+    with sr.Microphone() as source:
+        print("listening")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        print(command)
+except:
+    pass
+
+
